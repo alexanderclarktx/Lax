efe56415efee56eeb0059ca8def2964a54a74469
Alex <petrovich.alex96@gmail.com>
2025-08-27 11:40:53 -0700

    wip

--- docs/ball.js
+++ docs/ball.js
@@ -47,44 +47,47 @@ var LaxDiv = (props) => {
   return { e: div, update: props.update, state: props.state };
 };
 // src/library/Ball.ts
-var Ball = (color) => LaxDiv({
-  state: {
-    position: { x: 0, y: 0 },
-    velocity: { x: Math.random() * 2 - 4, y: Math.random() * 5 - 10 },
-    radius: 20,
-    gravity: 0.05
-  },
-  style: {
-    backgroundColor: color,
-    borderRadius: "50%",
-    border: "1px solid white"
-  },
-  update: (div, state) => {
-    div.style.width = `${state.radius * 2}px`;
-    div.style.height = `${state.radius * 2}px`;
-    div.style.top = `${state.position.y}px`;
-    div.style.left = `${state.position.x}px`;
-    state.velocity.y += state.gravity;
-    state.position.x += state.velocity.x;
-    state.position.y += state.velocity.y;
-    if (state.position.x < 0) {
-      state.position.x = 0;
-      state.velocity.x *= -1;
+var Ball = (color) => {
+  const ball = LaxDiv({
+    state: {
+      position: { x: 0, y: 0 },
+      velocity: { x: Math.random() * 2 - 4, y: Math.random() * 5 - 10 },
+      radius: 20,
+      gravity: 0.05
+    },
+    style: {
+      backgroundColor: color,
+      borderRadius: "50%",
+      border: "1px solid white"
+    },
+    update: (div, state) => {
+      div.style.width = `${state.radius * 2}px`;
+      div.style.height = `${state.radius * 2}px`;
+      div.style.top = `${state.position.y}px`;
+      div.style.left = `${state.position.x}px`;
+      state.velocity.y += state.gravity;
+      state.position.x += state.velocity.x;
+      state.position.y += state.velocity.y;
+      if (state.position.x < 0) {
+        state.position.x = 0;
+        state.velocity.x *= -1;
+      }
+      if (state.position.x + state.radius * 2 > window.innerWidth) {
+        state.position.x = window.innerWidth - state.radius * 2;
+        state.velocity.x *= -1;
+      }
+      if (state.position.y < 0) {
+        state.position.y = 0;
+        state.velocity.y *= -1;
+      }
+      if (state.position.y + state.radius * 2 > window.innerHeight) {
+        state.position.y = window.innerHeight - state.radius * 2;
+        state.velocity.y *= -1;
+      }
     }
-    if (state.position.x + state.radius * 2 > window.innerWidth) {
-      state.position.x = window.innerWidth - state.radius * 2;
-      state.velocity.x *= -1;
-    }
-    if (state.position.y < 0) {
-      state.position.y = 0;
-      state.velocity.y *= -1;
-    }
-    if (state.position.y + state.radius * 2 > window.innerHeight) {
-      state.position.y = window.innerHeight - state.radius * 2;
-      state.velocity.y *= -1;
-    }
-  }
-});
+  });
+  return ball;
+};
 // docs/ball.ts
 var lax = Lax({});
 lax.append(Ball("#00ffaa"));
--- src/library/Ball.ts
+++ src/library/Ball.ts
@@ -7,54 +7,59 @@ type BallState = {
   gravity: number
 }
 
-export const Ball = (color: LaxColor) => LaxDiv<BallState>({
-  state: {
-    position: { x: 0, y: 0 },
-    velocity: { x: Math.random() * 2 - 4, y: Math.random() * 5 - 10 },
-    radius: 20,
-    gravity: 0.05
-  },
-  style: {
-    backgroundColor: color,
-    borderRadius: "50%",
-    border: "1px solid white"
-  },
-  update: (div, state) => {
-    // shape
-    div.style.width = `${state.radius * 2}px`
-    div.style.height = `${state.radius * 2}px`
+export const Ball = (color: LaxColor): LaxDiv<BallState> => {
 
-    // position
-    div.style.top = `${state.position.y}px`
-    div.style.left = `${state.position.x}px`
+  const ball = LaxDiv<BallState>({
+    state: {
+      position: { x: 0, y: 0 },
+      velocity: { x: Math.random() * 2 - 4, y: Math.random() * 5 - 10 },
+      radius: 20,
+      gravity: 0.05
+    },
+    style: {
+      backgroundColor: color,
+      borderRadius: "50%",
+      border: "1px solid white"
+    },
+    update: (div, state) => {
+      // shape
+      div.style.width = `${state.radius * 2}px`
+      div.style.height = `${state.radius * 2}px`
 
-    // gravity
-    state.velocity.y += state.gravity
+      // position
+      div.style.top = `${state.position.y}px`
+      div.style.left = `${state.position.x}px`
 
-    // velocity
-    state.position.x += state.velocity.x
-    state.position.y += state.velocity.y
+      // gravity
+      state.velocity.y += state.gravity
 
-    // bounces
+      // velocity
+      state.position.x += state.velocity.x
+      state.position.y += state.velocity.y
 
-    if (state.position.x < 0) {
-      state.position.x = 0
-      state.velocity.x *= -1
+      // bounces
+
+      if (state.position.x < 0) {
+        state.position.x = 0
+        state.velocity.x *= -1
+      }
+
+      if (state.position.x + state.radius * 2 > window.innerWidth) {
+        state.position.x = window.innerWidth - state.radius * 2
+        state.velocity.x *= -1
+      }
+
+      if (state.position.y < 0) {
+        state.position.y = 0
+        state.velocity.y *= -1
+      }
+
+      if (state.position.y + state.radius * 2 > window.innerHeight) {
+        state.position.y = window.innerHeight - state.radius * 2
+        state.velocity.y *= -1
+      }
     }
+  })
 
-    if (state.position.x + state.radius * 2 > window.innerWidth) {
-      state.position.x = window.innerWidth - state.radius * 2
-      state.velocity.x *= -1
-    }
-
-    if (state.position.y < 0) {
-      state.position.y = 0
-      state.velocity.y *= -1
-    }
-
-    if (state.position.y + state.radius * 2 > window.innerHeight) {
-      state.position.y = window.innerHeight - state.radius * 2
-      state.velocity.y *= -1
-    }
-  }
-})
+  return ball
+}
